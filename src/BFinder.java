e69b7c16fd0b0738b7a0ae98a9fe8725af3bdd2a
Kiera <Kiiwii28@gmail.com>
2020-08-21 21:00:17 +0200

    Correct basin output

--- src/BFinder.java
+++ src/BFinder.java
@@ -5,6 +5,7 @@ import java.io.File;
 public class BFinder{
     public static void main(String [] args) throws FileNotFoundException{
             System.out.println("hello");
+            int iBasinCount = 0;
 
           String sFileIN = args[0];
           String sFileOUT = args[1];
@@ -19,6 +20,9 @@ public class BFinder{
           System.out.println(iRows + " = rows");
         System.out.println(iCols + " = Columns");
 
+        //for output purposes
+        String[] basinPositons = new String[iRows*iCols] ;
+
         String sPlots = input.nextLine();
         //System.out.println(sPlots);
          Scanner loadPlots = new Scanner(sPlots);
@@ -32,7 +36,7 @@ public class BFinder{
        // float test = sTest.valueOf();
        // float test = Float.parseFloat(sTest);
        // System.out.println(test + " = test.");
-        for(int i = 0; i<iRows; i++){
+        for(int i = 0; i<iRows; i++){ //populating matrix map full
             for (int j = 0; j<iCols; j++){
                 map[i][j] = Float.parseFloat(loadPlots.next());
                 //System.out.println(map[i][j]);
@@ -43,27 +47,42 @@ public class BFinder{
         }
 
         //System.out.println(map[0][3]);
-
+            System.out.println("only non-edges");
         for(int i = 1; i<iRows-1; i++) { //miss first row and last row, first column and last column
             for(int j = 1; j<iCols-1; j++){
                 System.out.print(map[i][j]+ " ");
+               // System.out.println();
 
                 //find each non-edge plot
                 //now check if basin by checking surrounding cells
+                //System.out.println("Checking neighbours");
                 int iHiNeighbours = 0;
-                for (int r = 0; r<2 ; r++){
-                    for(int c = 0; c<2 ; c++){
-                        
+                 // -1 .. 0 .. 1
+                for (int r = -1; r<2 ; r++){ //iterating through neighbours top row to one below
+                    // -1 .. 0 .. 1
+                 for(int c = -1; c<2 ; c++){ //1 left to 1 right
+                        if (map[i+r][j+c] > (map[i][j]+0.01)){
+                           // System.out.println(map[i+r][j+c] + " is Higher by 0.01 than " + map[i][j]);
+                            iHiNeighbours++;
+                        }
                     }
 
                 }
+                if (iHiNeighbours == 8){
+                    System.out.println(map[i][j] + "has full high neighbours.. 8");
+                    basinPositons[iBasinCount] = i + " " + j;
+                    iBasinCount++;
 
+                }
             }
             System.out.println();
             //System.out.println();
         }
-
+        System.out.println("number of basins = " + iBasinCount);
+        for (int i = 0; i<iBasinCount;i++){
+            System.out.println("Basin number: " + i + " at " + basinPositons[i]);
+        }
     }
 
 
-}
+};
